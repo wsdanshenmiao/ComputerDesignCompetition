35ce5aad62c46f59a813f0ea964b3271f83b746a
danshenmiao <wsdanshenmiao@outlook.com>
2025-03-23 14:46:42 +0800

    实现显示回答

--- Assets/Scripts/CraftingSystem/UI/ReminderAnswer.cs
+++ Assets/Scripts/CraftingSystem/UI/ReminderAnswer.cs
@@ -6,14 +6,21 @@ using UnityEngine;
 
 public class ReminderAnswer : MonoBehaviour
 {
+    // 回答问题后的图片展示
+    [SerializeField] private Transform correctAnswer;
+    [SerializeField] private Transform wrongAnswer;
+    
     private RecipeReminder reminder;
     
     private Transform selectTemplate;
 
+    [SerializeField] private List<TextAsset> selects = new List<TextAsset>();
+    
     public float textInterval = 100;
 
+    public float answerShowTime = 1;
+    
     public TextAsset correctSelect;
-    [SerializeField] List<TextAsset> selects = new List<TextAsset>();
     
     [HideInInspector] public TMP_Text currSelect;
 
@@ -32,6 +39,8 @@ public class ReminderAnswer : MonoBehaviour
             string text = (char)('A' + i) + ":" + selects[i].text;
             trans.GetComponentInChildren<TMP_Text>().text = text;
         }
+        correctAnswer.gameObject.SetActive(false);
+        wrongAnswer.gameObject.SetActive(false);
     }
 
     public void SetReminder(RecipeReminder reminder)
@@ -49,12 +58,25 @@ public class ReminderAnswer : MonoBehaviour
         currSelect = select;
     }
 
+    // 确定选项
     public void DetermineSelect()
     {
         if (currSelect != null && correctSelect.text.Equals(currSelect.text.Substring(2))) {
             reminder?.UnlockReminder();
+            StartCoroutine(AfterDetermineSelect(true));
         }
+        else {
+            StartCoroutine(AfterDetermineSelect(false));
+        }
+    }
+
+    // 显示回答结果并关闭
+    private IEnumerator AfterDetermineSelect(bool rightSelect)
+    {
+        Transform answer = rightSelect ? correctAnswer : wrongAnswer;
+        answer.gameObject.SetActive(true);
+        yield return new WaitForSeconds(answerShowTime);
+        answer.gameObject.SetActive(false);
         reminder?.CloseAnswerUI();
-        
     }
 }
